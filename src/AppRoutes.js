fd0292d1c439df30ef915dbbd671ecb96933bb09
Sasha Rodriguez Carlomagno <caballerosasha@hotmail.com>
2026-02-03 22:05:36 -0300

    vocabulario japones

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -11,29 +11,36 @@ import Error from './components/Error/Error.js';
 import Exito from './components/Exito/Exito.js';
 import Footer from "./components/Footer/Footer.js"; 
 import Videos from './components/Videos/Videos.js';
+import { lazy, Suspense } from 'react';
+
+const BuscadorJP = lazy(() => import('./components/BuscadorJP/BuscadorJP.js'));
 
 const AppRoutes = () => {
   const location = useLocation();
-  const hideNavbarOnRoutes = ['/'];
+  const hideNavbarOnRoutes = ['/', '/diccjp'];
   const showNavbar = !hideNavbarOnRoutes.includes(location.pathname);
 
   return (
     <div className="App">
       {showNavbar && <Navbar />}
       <main className='MainContent' style={{ flexGrow: 1 }}>
-        <Routes>
-          <Route path='/' element={<Start />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/validar' element={<Validar />} />
-          <Route path='/crearfecha' element={<CrearFecha fechaEditar={null} setFechaEditar={null}/>} />
-          <Route path='/fecha/:id' element={<Fecha/>} />
-          <Route path='/vtaentradas' element={<VentaEntradas/>} />
-          <Route path='/success' element={<Exito/>} />
-          <Route path='/error' element={<Error/>} />
-          <Route path='/videos' element={<Videos />} />
-          <Route path='*' element={<h1>Not Found</h1>} />
-        </Routes>
+        <Suspense fallback={<div>Cargando...</div>}>
+          <Routes>
+            <Route path='/' element={<Start />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/validar' element={<Validar />} />
+            <Route path='/crearfecha' element={<CrearFecha fechaEditar={null} setFechaEditar={null}/>} />
+            <Route path='/fecha/:id' element={<Fecha/>} />
+            <Route path='/vtaentradas' element={<VentaEntradas/>} />
+            <Route path='/success' element={<Exito/>} />
+            <Route path='/error' element={<Error/>} />
+            <Route path='/videos' element={<Videos />} />
+            <Route path='/diccjp' element={<BuscadorJP />} />
+            <Route path='*' element={<h1>Not Found</h1>} />
+          </Routes>
+        </Suspense>
+
       </main>
       {showNavbar && <Footer />}
     </div>
